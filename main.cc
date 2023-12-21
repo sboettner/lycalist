63869ca50427eb9e62d4c1a125155b3fd7e2a7f2
Stefan T. Boettner <git@github.com:sboettner/lycalist.git>
2023-12-21 22:30:25 +0100

    new class for main window

--- main.cc
+++ main.cc
@@ -3,6 +3,25 @@
 #include "lyricedit.h"
 
 
+class MainWindow:public Gtk::Window {
+public:
+    MainWindow();
+
+private:
+    LyricEditor lyric_editor;
+};
+
+
+MainWindow::MainWindow()
+{
+    set_default_size(960, 640);
+
+    add(lyric_editor);
+
+    show_all_children();
+}
+
+
 int main(int argc, char* argv[])
 {
     auto app=Gtk::Application::create(argc, argv);
@@ -10,15 +29,7 @@ int main(int argc, char* argv[])
     auto settings=Gtk::Settings::get_default();
     settings->property_gtk_application_prefer_dark_theme()=true;
 
-    Gtk::Window wnd;
-	wnd.add_events(Gdk::KEY_PRESS_MASK);
-
-    LyricEditor editor;
-    wnd.add(editor);
-    editor.show();
-
-    wnd.set_default_size(960, 640);
-    wnd.show_all_children();
+    MainWindow wnd;
 
     return app->run(wnd);
 }
